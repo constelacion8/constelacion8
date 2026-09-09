e8694e9e6f4451fc53c529f62748771be248c983
constelacion8 <info@enriquerodda.com>
2026-09-09 13:49:53 +0100

    Load municipal status overlay

--- m88/config.js
+++ m88/config.js
@@ -3,4 +3,5 @@ export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_AjQQNYKbCwaGNv_o1GopAw_T
 
 if (typeof document !== 'undefined') {
   import('./agenda.js?v=20260909-1').catch(error=>console.warn('Agenda no disponible',error));
+  import('./municipal-map.js?v=20260909-municipios1').catch(error=>console.warn('Mapa municipal no disponible',error));
 }
