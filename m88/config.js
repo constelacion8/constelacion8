cc6a219dc0561b72ace74b9e0dcb6b880b701378
constelacion8 <info@enriquerodda.com>
2026-09-09 11:16:38 +0100

    Load confirmed meetings agenda on home

--- m88/config.js
+++ m88/config.js
@@ -1,2 +1,6 @@
 export const SUPABASE_URL = 'https://kvoldyeinvjajsimxmyc.supabase.co';
 export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_AjQQNYKbCwaGNv_o1GopAw_TzyLkHXh';
+
+if (typeof document !== 'undefined') {
+  import('./agenda.js?v=20260909-1').catch(error=>console.warn('Agenda no disponible',error));
+}
