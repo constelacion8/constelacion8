ba4f97b539efe180e26ba1a44a72042879156baa
constelacion8 <info@enriquerodda.com>
2026-09-11 21:22:03 +0000

    Connect Devolucion Cultural module to M88

--- m88/config.js
+++ m88/config.js
@@ -15,6 +15,7 @@ if (typeof document !== 'undefined' && nativeFetch) {
 
   import('./agenda.js?v=20260910-private1').catch(error=>console.warn('Agenda no disponible',error));
   import('./municipal-map.js?v=20260910-private1').catch(error=>console.warn('Mapa municipal no disponible',error));
+  import('./companies.js?v=20260911-private1').catch(error=>console.warn('Devolución Cultural no disponible',error));
 }
 
 function installPrivateFetchProxy(){
